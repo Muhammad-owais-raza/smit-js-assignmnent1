249fac0a77cf69a1730940e3486f928ddbf4005c
Muhammad-owais-raza <owaisraza.codes@gmail.com>
2026-06-15 02:03:24 +0500

    my first smit js assignment completed

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+let firstAlert = "Welcome to JS and Let's add 2 numbers";
+
+
+alert(firstAlert);
+
+
+
+
+// This is for Addition
+
+let number1 = Number(prompt("Enter first number"));
+
+let number2 = Number(prompt("Enter second number"));
+
+let resultAddition = number1 + number2;
+
+alert("Addition result :  " + resultAddition);
+
+document.write(resultAddition + "<br><br>" );
+
+
+
+
+// This is for Persantage
+
+let number_1 = Number(prompt("Enter first number"));
+
+let number_2 = Number(prompt("Enter second number"));
+
+let division = number_1 / number_2;
+
+let resultPersantage = division * 100 ;
+
+alert("Persantage result :  " + resultPersantage);
+
+document.write(resultPersantage);
